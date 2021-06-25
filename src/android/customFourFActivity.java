61d06fdfcf6a71dbfd10b6c38dbf864b8d2bb5a0
Jean Carlo <jeanctazza@gmail.com>
2021-06-25 10:00:02 -0500

    Actualizacion de Librerias

--- src/android/customFourFActivity.java
+++ src/android/customFourFActivity.java
@@ -6,19 +6,18 @@ import android.util.Log;
 
 import android.support.v4.app.Fragment;
 
-import com.veridiumid.sdk.defaultdata.DataStorage;
 import com.veridiumid.sdk.fourf.FourFInterface;
 import com.veridiumid.sdk.fourf.FourFBiometricsActivity;
 import com.veridiumid.sdk.fourf.FourFUIInterface;
-import com.veridiumid.sdk.fourf.ui.authenticator.AuthenticatorFourFFragment;
-import com.veridiumid.sdk.model.biometrics.persistence.impl.BytesTemplatesStorage;
 import com.veridiumid.sdk.model.data.persistence.IKVStore;
+import com.veridiumid.sdk.model.data.persistence.impl.InMemoryKVStore;
 
 /**
  *  Extends vFaceBiometricsActivity to set storage to use and which UI to show.
  */
 public class customFourFActivity extends FourFBiometricsActivity {
 
+/**
     private String TAG = "customFourFActivity";
 
     @Override protected <FourFFragmentInterfaceUnion extends Fragment & FourFUIInterface> FourFFragmentInterfaceUnion fragmentToShow(){
@@ -45,5 +44,16 @@ public class customFourFActivity extends FourFBiometricsActivity {
             return templates_store.isEnrolled();
         }
         return false;
-    }
+    } */
+
+    @Override
+       protected <FourFFragmentInterfaceUnion extends Fragment & FourFUIInterface> FourFFragmentInterfaceUnion fragmentToShow() {
+           return (FourFFragmentInterfaceUnion) new FourFUIFragment();
+       }
+
+        @Override
+        protected IKVStore openStorage() {
+            return new InMemoryKVStore();
+        }
+
 }
